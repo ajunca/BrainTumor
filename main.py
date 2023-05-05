e2cb94d7082c22aaf82f50eba76b99ebc0bee840
Albert Juncà Sabrià <albert.junca.sabria@gmail.com>
2023-05-05 11:09:40 +0200

    Metastability computation and some plotting.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # This is a sample Python script.
-import BOLDFilters
+# import BOLDFilters
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import WholeBrain.Utils.plotSC
+# import WholeBrain.Utils.plotSC
 from setup import *
 # import WholeBrain.Observables.FC as FCObservable
 # import WholeBrain.Observables.Metastability as MSObservable
